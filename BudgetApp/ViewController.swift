c08b20a2aa9ca89f4b35a9c1c41b1665c853eaac
Zineb El mechrafi <elmechrafizineb@hotmail.com>
2016-04-09 16:28:54 -0400

    Initial Commit

---
+++ BudgetApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by Zineb El mechrafi on 4/9/16.
+//  Copyright © 2016 Zineb El Mechrafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
