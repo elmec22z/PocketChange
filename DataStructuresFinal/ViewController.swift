d96ee41432fa6cea96951ffba1d1c94c16c37369
Zineb El mechrafi <elmechrafizineb@hotmail.com>
2016-04-09 16:37:15 -0400

    Zineb

---
+++ DataStructuresFinal/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  FirstViewController.swift
+//  Final Project
+//
+//  Created by Zineb El mechrafi on 3/30/16.
+//  Copyright © 2016 Zineb El mechrafi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    //MARKS: Fields
+    @IBOutlet var firstField: UITextField!
+    @IBOutlet var secondField: UITextField!
+    @IBOutlet var thirdField: UITextField!
+    @IBOutlet var fourthField: UITextField!
+    @IBOutlet var totalLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func compute(sender: AnyObject) {
+        
+        let number1 = Int(firstField.text!)
+        
+        let number2 = Int(secondField.text!)
+        
+        let number3 = Int(thirdField.text!)
+        
+        let number4 = Int(fourthField.text!)
+
+        let duration = number1! + number2! + number3! + number4!
+        
+        totalLabel.text = "\(duration)"
+    }
+    
+}
+
+
