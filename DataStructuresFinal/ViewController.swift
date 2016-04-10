6d321c30d66de500ba84a8d87e3c153b7e264542
Zineb El mechrafi <elmechrafizineb@hotmail.com>
2016-04-09 20:22:45 -0400

    Add receipt view controller added

--- DataStructuresFinal/ViewController.swift
+++ DataStructuresFinal/ViewController.swift
@@ -15,17 +15,22 @@ class FirstViewController: UIViewController {
     @IBOutlet var thirdField: UITextField!
     @IBOutlet var fourthField: UITextField!
     @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var myPicker: UIPickerView!
+    @IBOutlet var myTextField: UITextField!
+    var pickerData: [String] = [String]()
+
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //Dispose of any resources that can be recreated.
     }
     
     @IBAction func compute(sender: AnyObject) {
